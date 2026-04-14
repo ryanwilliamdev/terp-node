5860b65d74dad4c6cd072c59a1347cf1a21df5c1
raindevice <rywilldav@gmail.com>
2026-04-13 20:43:55 -0400

    update gitignore to include .env variables, add listen port to server.js

--- server.js
+++ server.js
@@ -1,56 +1,58 @@
 const express = require('express')
+const dotenv = require('dotenv')
+
+// Load config
+dotenv.config({ path: './config' })
+
 const app = express()
-const cors = require('cors')
-const PORT = 6666
 
-app.use(cors())
+const PORT = process.env.PORT || 6666
 
-const terpeneData = {
-    'unknown': {
-        id: 0,
-        'aroma': 'unknown',
-        'effects': 'unknown',
-    },
-    'myrcene': {
-        id: 1,
-        'aroma': 'Earthy, Musky, Cloves',
-        'effects': ['Relaxing', 'Sedating'],
-    },
-    'limonene': {
-        id: 2,
-        'aroma': 'Citrus, Lemon, Orange',
-        'effects': ['Mood Elevation', 'Stress Relief'],
-    },
-    'caryophyllene': {
-        id: 3,
-        'aroma': 'Peppery, Spicy, Wood',
-        'effects': ['Anti-inflammatory', 'Anxiety Relief'],
-    },
-    'linalool': {
-        id: 4,
-        'aroma': 'Floral, Lavender',
-        'effects': ['Calming', 'Sleep Aid'],
-    },
-    'pinene': {
-        id: 5,
-        'aroma': 'Pine, Sharp, Sweet',
-        'effects': ['Alertness', 'Memory Retention'],
-    },
-}
+app.listen(
+    PORT,
+    console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+)
 
-app.get('/', (request, response) => {
-    response.sendFile(__dirname + '/index.html')
-})
 
-app.get('/api/:name', (request, response) => {
-    const terpeneName = request.params.name.toLowerCase()
-    if (terpeneData[terpeneName]) {
-        response.json(terpeneData[terpeneName])
-    } else {
-        response.json(terpeneData['unknown'])
-    }
-})
 
-app.listen(process.env.PORT || PORT, () => {
-    console.log(`The server is now running on port: ${PORT}.`)
-})
+
+
+
+
+
+
+// const terpeneData = {
+//     'unknown': {
+//         id: 0,
+//         'aroma': 'unknown',
+//         'effects': 'unknown',
+//     },
+//     'myrcene': {
+//         id: 1,
+//         'aroma': 'Earthy, Musky, Cloves',
+//         'effects': ['Relaxing', 'Sedating'],
+//     },
+//     'limonene': {
+//         id: 2,
+//         'aroma': 'Citrus, Lemon, Orange',
+//         'effects': ['Mood Elevation', 'Stress Relief'],
+//     },
+//     'caryophyllene': {
+//         id: 3,
+//         'aroma': 'Peppery, Spicy, Wood',
+//         'effects': ['Anti-inflammatory', 'Anxiety Relief'],
+//     },
+//     'linalool': {
+//         id: 4,
+//         'aroma': 'Floral, Lavender',
+//         'effects': ['Calming', 'Sleep Aid'],
+//     },
+//     'pinene': {
+//         id: 5,
+//         'aroma': 'Pine, Sharp, Sweet',
+//         'effects': ['Alertness', 'Memory Retention'],
+//     },
+// }
+
+
+
