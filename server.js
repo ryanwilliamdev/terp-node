33ef2f3d351ece786e0a9e70e2788c7f407fdf6f
ryanwilliamdev <rywilldav@gmail.com>
2026-05-04 13:52:52 -0400

    initial upload of public folder to house all static render/style files.

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const path = require('path')
 const express = require('express')
 const dotenv = require('dotenv')
 const morgan = require('morgan')
@@ -27,6 +28,9 @@ app.engine(
     })
 )
 app.set('view engine', 'hbs')
+
+// Static folder
+app.use(express.static(path.join(__dirname, 'public')))
 
 // Routes
 app.use('/', require('./routes/index'))
