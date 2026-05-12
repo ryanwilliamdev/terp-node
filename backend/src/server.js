d1efd5fa619126ae64cd4aafa6e8f5714dacabd7
ryanwilliamdev <rywilldav@gmail.com>
2026-05-12 14:42:16 -0400

    add config folder to store .env, install and configured mongoose and tied it to our mongodb database

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,10 +1,15 @@
 import express from 'express';
 import strainRoutes from './routes/strainRoutes.js';
+import { connectDB } from './config/db.js';
+import dotenv from 'dotenv'
+
+dotenv.config();
 
 const app = express();
 
 app.use('/api/strains', strainRoutes);
 
+connectDB();
 
 app.listen(3000, () => {
     console.log('Server started on PORT: 3000');
