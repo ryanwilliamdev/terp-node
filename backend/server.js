c6fa3be0a58b70cf66abdc90c767e6888c68fd07
ryanwilliamdev <rywilldav@gmail.com>
2026-05-12 10:09:38 -0400

    update package.json type to 'module' instead of 'commonjs', update server.js with a test get request on our future api route.

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,11 @@
-import express from 'express'
+import express from 'express';
 
-const app = express()
+const app = express();
+
+app.get('/api/strains', (req, res) => {
+    res.send('you got 5 notes')
+})
 
 app.listen(3000, () => {
-    console.log('Server started on PORT: 3000')
-})
+    console.log('Server started on PORT: 3000');
+});
