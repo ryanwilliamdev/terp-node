1619b9ec58778159ea1ad10acb0af4c2b347da5d
ryanwilliamdev <rywilldav@gmail.com>
2026-05-12 12:24:39 -0400

    update strainRoutes.js, added put and delete routes, updated the get router response.

--- backend/routes/strainRoutes.js
+++ backend/routes/strainRoutes.js
@@ -3,18 +3,24 @@ import express from 'express';
 const router = express.Router()
 
 router.get('/', (req, res) => { // READ
-    res.status(200).send('Here are your current favorite strains!');
+    res.status(200).send('Here are your current favorite cannabis strains!');
 });
 
 router.post('/', (req, res) => { // CREATE
     res.status(201).json({ message: 'Strain added successfully!' })
 })
 
+router.put('/', (req, res) => { // UPDATE
+    res.status(200).json({ message: 'Strain updated successfully!' });
+});
+
+router.delete('/', (req, res) => { // DELETE
+    res.status(200).json({ message: 'Strain deleted successfully!' })
+})
+
 export default router
 
-// app.put('/api/strains:id', (req, res) => { // UPDATE
-//     res.status(200).json({ message: 'Strain updated successfully!' });
-// });
+
 
 // app.delete('/api/strains:id', (req, res) => { // DELETE
 //     res.status(200).json({ message: 'Strain deleted successfully!' });
