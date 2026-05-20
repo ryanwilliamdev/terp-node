463653148307e5ecf7ea74d38752176b0fa7fe18
ryanwilliamdev <rywilldav@gmail.com>
2026-05-20 13:13:14 -0400

    fetch strains data in HomePage component using axios and useEffect for improved data management

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,9 +1,28 @@
 import React, { useState } from "react";
 import Navbar from "../components/Navbar";
 import RateLimitedUI from "../components/RateLimitedUI";
+import { useEffect } from "react";
+import axios from "axios";
 
 const HomePage = () => {
     const [isRateLimited, setRateLimited] = useState(false);
+    const [strains, setStrains] = useState([]);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        const fetchStrains = async () => {
+            try {
+                const res = await axios.get(
+                    "http://localhost:3000/api/strains",
+                );
+                console.log(res.data);
+            } catch (error) {
+                console.log("Error fetching strains.");
+            }
+        };
+
+        fetchStrains();
+    }, []);
     return (
         <div className="min-h-screen">
             <Navbar />
