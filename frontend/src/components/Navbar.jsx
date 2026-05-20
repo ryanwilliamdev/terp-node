fc8433767f0c1e7c556c2ec00b8322095e67a991
ryanwilliamdev <rywilldav@gmail.com>
2026-05-20 10:46:40 -0400

    refactor Navbar component to improve layout by repositioning the create link with icon

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -7,13 +7,13 @@ const Navbar = () => {
         <div className="mx-auto max-w-6xl p-4">
             <div className="flex items-center justify-between">
                 <h1 className="text-3xl font-bold text-primary font-mono tracking-tight">
-                    <div className="flex items-center gap-4">
-                        <Link to={"/create"} className="btn btn-primary">
-                            <PlusIcon />
-                        </Link>
-                    </div>
                     TerpNode
                 </h1>
+                <div className="flex items-center gap-4">
+                    <Link to={"/create"} className="btn btn-primary">
+                        <PlusIcon />
+                    </Link>
+                </div>
             </div>
         </div>
 
