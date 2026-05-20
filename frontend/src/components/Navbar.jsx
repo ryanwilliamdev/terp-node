a8af4944f353e620c9ac7b6189730522ec238562
ryanwilliamdev <rywilldav@gmail.com>
2026-05-20 10:45:52 -0400

    add create link with icon to Navbar for enhanced navigation

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,10 +1,17 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router';
+import { PlusIcon } from 'lucide-react';
 
 const Navbar = () => {
     return <header className="bg-base-300 border-b border-base-content/10">
         <div className="mx-auto max-w-6xl p-4">
             <div className="flex items-center justify-between">
                 <h1 className="text-3xl font-bold text-primary font-mono tracking-tight">
+                    <div className="flex items-center gap-4">
+                        <Link to={"/create"} className="btn btn-primary">
+                            <PlusIcon />
+                        </Link>
+                    </div>
                     TerpNode
                 </h1>
             </div>
