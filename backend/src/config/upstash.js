23eca7d1824828fdb276bce77c088508a00e5b2f
ryanwilliamdev <rywilldav@gmail.com>
2026-05-26 10:04:26 -0400

    Refactor upstash.js to update rate limiting configuration and standardize import quotes

--- backend/src/config/upstash.js
+++ backend/src/config/upstash.js
@@ -1,14 +1,14 @@
-import { Ratelimit } from '@upstash/ratelimit';
-import { Redis } from '@upstash/redis';
+import { Ratelimit } from "@upstash/ratelimit";
+import { Redis } from "@upstash/redis";
 
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
 
 dotenv.config();
 
 // create a rate limit instance with a sliding window of 20 seconds and a limit of 10 requests
 const ratelimit = new Ratelimit({
     redis: Redis.fromEnv(),
-    limiter: Ratelimit.slidingWindow(100, '60 s'),
+    limiter: Ratelimit.slidingWindow(10, "20 s"),
 });
 
 export default ratelimit;
