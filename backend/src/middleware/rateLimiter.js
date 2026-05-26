17e375958d9155aa083dc4e6b5dc37847f928ea9
ryanwilliamdev <rywilldav@gmail.com>
2026-05-26 11:51:21 -0400

    Update rateLimiter middleware to use a fixed limit key for request tracking

--- backend/src/middleware/rateLimiter.js
+++ backend/src/middleware/rateLimiter.js
@@ -2,7 +2,7 @@ import ratelimit from "../config/upstash.js";
 
 const rateLimiter = async (req, res, next) => {
     try {
-        const { success } = await ratelimit.limit(req.ip);
+        const { success } = await ratelimit.limit("my-limit-key");
         if (!success) {
             return res.status(429).json({ message: "Too many requests" });
         }
