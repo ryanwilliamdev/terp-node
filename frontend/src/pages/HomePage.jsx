e5d02b26f0f7ff59667d91c5852a5c078f89a35d
ryanwilliamdev <rywilldav@gmail.com>
2026-05-26 13:39:30 -0400

    Fix rendering of StrainCard components in HomePage by using parentheses for proper JSX syntax

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -48,9 +48,9 @@ const HomePage = () => {
                 )}
                 {strains.length > 0 && !isRateLimited && (
                     <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
-                        {strains.map((strain) => {
-                            <StrainCard key={strain._id} strain={strain} />;
-                        })}
+                        {strains.map((strain) => (
+                            <StrainCard key={strain._id} strain={strain} />
+                        ))}
                     </div>
                 )}
             </div>
