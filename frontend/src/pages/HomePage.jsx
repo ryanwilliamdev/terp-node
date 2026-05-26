d8e93d4168674fa5b9e5fa0f4c704a04d7342162
ryanwilliamdev <rywilldav@gmail.com>
2026-05-26 12:10:22 -0400

    Add loading state and strain display in HomePage component

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -39,6 +39,20 @@ const HomePage = () => {
             <Navbar />
 
             {isRateLimited && <RateLimitedUI />}
+            <div className="max-w-7xl mx-auto p-4 mt-6">
+                {loading && (
+                    <div className="text-center text-primary py-10">
+                        Loading strains...
+                    </div>
+                )}
+                {strains.length > 0 && !isRateLimited && (
+                    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+                        {strains.map((strain) => {
+                            <StrainCard key={strain._id} strain={strain} />;
+                        })}
+                    </div>
+                )}
+            </div>
         </div>
     );
 };
