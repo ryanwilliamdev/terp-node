33c732a07b37350be3fe3397c83e6fbd9d95da6a
ryanwilliamdev <rywilldav@gmail.com>
2026-05-26 13:05:42 -0400

    Refactor Strain and StrainNote schemas to standardize string quotes and improve schema structure with timestamps

--- backend/src/models/Strain.js
+++ backend/src/models/Strain.js
@@ -1,41 +1,47 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
-const strainSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        required: true,
+const strainSchema = new mongoose.Schema(
+    {
+        name: {
+            type: String,
+            required: true,
+        },
+        type: {
+            type: String,
+            required: true,
+        },
+        cultivator: {
+            type: String,
+            required: true,
+        },
+        terpenes: {
+            type: String,
+            required: true,
+        },
+        flavor: {
+            type: String,
+            required: true,
+        },
     },
-    type: {
-        type: [String],
-        required: true,
-    },
-    cultivator: {
-        type: String,
-        required: true,
-    },
-    terpenes: {
-        type: [String],
-        required: true,
-    },
-    flavor: {
-        type: [String],
-        required: true,
-    },
-});
+    { timestamps: true }, // createdAt, updatedAt
+);
 
-const strainNoteSchema = new mongoose.Schema({
-    strain: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'Strain',
-        required: true,
+const strainNoteSchema = new mongoose.Schema(
+    {
+        strain: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "Strain",
+            required: true,
+        },
+        note: {
+            type: String,
+            required: true,
+        },
     },
-    note: {
-        type: String,
-        required: true,
-    },
-}, { timestamps: true }); // createdAt and updatedAt
+    { timestamps: true },
+); // createdAt and updatedAt
 
-const Strain = mongoose.model('Strain', strainSchema);
-const StrainNote = mongoose.model('StrainNote', strainNoteSchema);
+const Strain = mongoose.model("Strain", strainSchema);
+const StrainNote = mongoose.model("StrainNote", strainNoteSchema);
 
 export { Strain, StrainNote };
