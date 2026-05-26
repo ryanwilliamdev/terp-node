ea24535af0f96ed851957e0e1f1decf1dd9e3fbb
ryanwilliamdev <rywilldav@gmail.com>
2026-05-26 09:49:00 -0400

    Add error handling in HomePage for strain fetching, including rate limit management and user notifications using react hot toast

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -3,6 +3,8 @@ import Navbar from "../components/Navbar";
 import RateLimitedUI from "../components/RateLimitedUI";
 import { useEffect } from "react";
 import axios from "axios";
+import toast from "react-hot-toast";
+import { TableRowsSplit } from "lucide-react";
 
 const HomePage = () => {
     const [isRateLimited, setRateLimited] = useState(false);
@@ -16,8 +18,17 @@ const HomePage = () => {
                     "http://localhost:3000/api/strains",
                 );
                 console.log(res.data);
+                setStrains(res.data);
+                setIsRateLimited(false);
             } catch (error) {
                 console.log("Error fetching strains.");
+                if (error.response.status === 429) {
+                    setIsRateLimited(true);
+                } else {
+                    toast.error("Failed to load strains.");
+                }
+            } finally {
+                setLoading(false);
             }
         };
 
