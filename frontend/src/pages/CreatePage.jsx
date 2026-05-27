ae1383a4583919065586292aa14eef5ac1ab8095
ryanwilliamdev <rywilldav@gmail.com>
2026-05-27 11:53:16 -0400

    Implement CreatePage layout with form state management and navigation link

--- frontend/src/pages/CreatePage.jsx
+++ frontend/src/pages/CreatePage.jsx
@@ -1,8 +1,28 @@
-import React from 'react';
+import { ArrowLeftIcon } from "lucide-react";
+import React, { useState } from "react";
+import { Link } from "react-router";
 
 const CreatePage = () => {
+    const [name, setName] = useState("");
+    const [type, setType] = useState("");
+    const [cultivator, setCultivator] = useState("");
+    const [terpenes, setTerpenes] = useState("");
+    const [flavor, setFlavor] = useState("");
+    const [loading, setLoading] = useState(false);
+
+    const handleSubmit = () => {};
+
     return (
-        <div>CreatePage</div>
+        <div className="min-h-screen bg-base-200">
+            <div className="container mx-auto px-4 py-8">
+                <div className="max-w-2xl mx-auto">
+                    <Link to={"/"} className="btn btn-ghost mb-6">
+                        <ArrowLeftIcon className="size-5" />
+                        Back to Strains
+                    </Link>
+                </div>
+            </div>
+        </div>
     );
 };
 
