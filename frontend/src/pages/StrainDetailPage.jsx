e90751420544cd5b544b386fa8e5b1726a4d630c
ryanwilliamdev <rywilldav@gmail.com>
2026-06-01 13:31:29 -0400

    Add strain detail fetching logic to StrainDetailPage component, including loading state and error handling with toast notifications.

--- frontend/src/pages/StrainDetailPage.jsx
+++ frontend/src/pages/StrainDetailPage.jsx
@@ -1,7 +1,31 @@
-const StrainDetailPage = () => {
-    return (
-        <div>StrainDetailPage</div>
-    )
-}
+import { useState, useEffect } from "react";
+import { useNavigate, useParams } from "react-router";
+import api from "../lib/axios";
+import toast from "react-hot-toast";
 
-export default StrainDetailPage
+const StrainDetailPage = () => {
+    const [strain, setStrain] = useState(null);
+    const [loading, setLoading] = useState(true);
+    const [saving, setSaving] = useState(false);
+    const navigate = useNavigate();
+    const { id } = useParams();
+    useEffect(() => {
+        const fetchStrain = async () => {
+            try {
+                const res = await api.get(`/strains/${id}`);
+                setStrain(res.data);
+            } catch (error) {
+                console.log("Error fetching strain:", error);
+                toast.error("Failed to load strain.");
+            } finally {
+                setLoading(false);
+            }
+        };
+        fetchStrain();
+    }, [id]);
+
+    console.log({ strain });
+    return <div>StrainDetailPage</div>;
+};
+
+export default StrainDetailPage;
