3122edfd11264913b3b77668445f6168a3b3a0c0
ryanwilliamdev <rywilldav@gmail.com>
2026-06-08 13:25:43 -0400

    Enhance strainRoutes by adding getStrainById endpoint and improving import organization for better readability.

--- backend/src/routes/strainRoutes.js
+++ backend/src/routes/strainRoutes.js
@@ -1,12 +1,18 @@
-import express from 'express';
-import { addStrain, getAllStrains, updateStrain, deleteStrain } from '../controllers/strainController.js';
+import express from "express";
+import {
+    addStrain,
+    getAllStrains,
+    getStrainById,
+    updateStrain,
+    deleteStrain,
+} from "../controllers/strainController.js";
 
 const router = express.Router();
 
-router.get('/', getAllStrains);
-// router.get('/:id', getStrainById)
-router.post('/', addStrain);
-router.put('/:id', updateStrain);
-router.delete('/:id', deleteStrain);
+router.get("/", getAllStrains);
+router.get("/:id", getStrainById);
+router.post("/", addStrain);
+router.put("/:id", updateStrain);
+router.delete("/:id", deleteStrain);
 
 export default router;
